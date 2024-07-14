1160376e35b336cfca1f955feb7b9731da9eca81
bikash1376 <139378098+bikash1376@users.noreply.github.com>
2024-07-14 19:15:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:4000/",
+        target: "https://kataaksh-backend.onrender.com/",
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ""),
       },
